148d016c5f85c1b842cd82cd6fc1582ce36d2b03
Nicolas Sanchez <nicolassancheztobar@hotmail.com>
2024-05-21 18:59:44 -0500

    Agregar archivos de proyecto.

---
+++ Data/BibliotecaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
